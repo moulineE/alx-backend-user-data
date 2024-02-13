e3cfabab36427d80dab1c952d2caf9c71cb998db
moulineE <elmahdi.mouline@live.fr>
2024-02-13 21:29:10 +0100

    Task 10. Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ Basic Auth module
 import base64
 
 from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -66,3 +68,23 @@ class BasicAuth(Auth):
                 if len(user_credentials) == 2:
                     return tuple(user_credentials)
         return None, None
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        return the user instance
+        Args:
+            user_email:
+            user_pwd:
+
+        Returns:
+            a User instance based on his email and password
+        """
+        if user_email and user_pwd:
+            if type(user_email) == str and type(user_pwd) == str:
+                users = User.search({'email': user_email})
+                if len(users) > 0:
+                    for user in users:
+                        if user.is_valid_password(user_pwd):
+                            return user
+        return None
