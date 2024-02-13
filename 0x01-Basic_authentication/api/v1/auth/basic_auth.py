2d33d67a78de69944c2b67d0960ce24e42160b8c
moulineE <elmahdi.mouline@live.fr>
2024-02-13 20:16:47 +0100

    Task 8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """
 Basic Auth module
 """
+import base64
+
 from api.v1.auth.auth import Auth
 
 
@@ -23,4 +25,25 @@ class BasicAuth(Auth):
             if type(authorization_header) == str:
                 if authorization_header.startswith('Basic '):
                     return authorization_header.split(' ')[1]
+        return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        a method that decode a Base64 string
+        Args:
+            base64_authorization_header:
+
+        Returns:
+            decoded value of a Base64 string
+
+        """
+        if base64_authorization_header:
+            if type(base64_authorization_header) == str:
+                try:
+                    return (base64.b64decode(
+                        base64_authorization_header, validate=True)
+                            .decode('utf-8'))
+                except (Exception):
+                    return None
         return None
