00da7b8e25f34871650405df7c47796d096a8ed7
moulineE <elmahdi.mouline@live.fr>
2024-02-13 17:06:08 +0100

    Task 5. Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -40,7 +40,9 @@ class Auth:
         Returns:
 
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
