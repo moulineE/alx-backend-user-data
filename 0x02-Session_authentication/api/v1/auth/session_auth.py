71cb46b7bfc2bf36a7ed381702767a60373434f8
moulineE <elmahdi.mouline@live.fr>
2024-02-14 19:07:42 +0100

    Task 6. Use Session ID for identifying a User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ SessionAuth module
 """
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -27,3 +28,11 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+
+        """
+        SessionID = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(SessionID)
+        return User.get(user_id)
