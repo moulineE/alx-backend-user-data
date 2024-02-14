20b3dab406d74ab7fc592a64c686eb9d9fd5e3e8
moulineE <elmahdi.mouline@live.fr>
2024-02-14 18:35:36 +0100

    Task 4. Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,8 @@
 """
 Auth module
 """
+import os
+
 from flask import request
 from typing import List, TypeVar
 import fnmatch
@@ -55,3 +57,12 @@ class Auth:
 
         """
         return None
+
+    def session_cookie(self, request=None):
+        """
+        returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
