b76f2a24544d17cfc067c955ae128be23ec9322f
moulineE <elmahdi.mouline@live.fr>
2024-02-19 05:47:08 +0100

    Task 1. create user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,50 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password):
+        """
+        Add a user in the db
+        :param email:
+        :param hashed_password:
+        :return:
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self.new(new_user)
+        self.save()
+        return new_user
+
+    def new(self, obj):
+        """Add the object to the current database session"""
+        self._session.add(obj)
+
+    def save(self):
+        """Commit all changes of the current database session"""
+        self._session.commit()
