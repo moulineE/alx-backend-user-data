87f01527897fe956b124cd44ed28bad78113d379
moulineE <elmahdi.mouline@live.fr>
2024-02-20 15:40:13 +0100

    Task 14. Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask app"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -38,6 +38,18 @@ def login():
         return resp
     else:
         abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """log out route"""
+    sessionID = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(sessionID)
+    if user:
+        AUTH.destroy_session(user.id)
+        redirect('/')
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
