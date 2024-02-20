fc5ec1d88893cc6822f14808a1d6a4593460f8f0
moulineE <elmahdi.mouline@live.fr>
2024-02-20 17:53:24 +0100

    Task 18. Update password --fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -106,7 +106,7 @@ class Auth:
             hashed_password = _hash_password(password)
             self._db.update_user(user.id, hashed_password=hashed_password,
                                  reset_token=None)
-        except NoResultFound:
+        except Exception:
             raise ValueError
 
 
