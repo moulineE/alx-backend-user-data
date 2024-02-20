66a12720551154b0abda1b083001eb3c058d9815
moulineE <elmahdi.mouline@live.fr>
2024-02-20 17:16:56 +0100

    Task 16. Generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,6 +80,20 @@ class Auth:
                 pass
         return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        method that reset the user password token
+        :param email:
+        :return:
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """
