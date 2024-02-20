0ab4ce09ddb0094c3512a0d2470824570b4057b5
moulineE <elmahdi.mouline@live.fr>
2024-02-20 20:17:30 +0100

    Task 18. Update password --fix16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -102,7 +102,7 @@ class Auth:
         :return:
         """
         if reset_token is None or password is None:
-            return None
+            raise ValueError
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             n_hashed_password = _hash_password(password)
