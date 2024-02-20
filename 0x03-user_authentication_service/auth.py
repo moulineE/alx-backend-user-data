d75e0f8eab04a78d2f7da52c37d314c1eafe4286
moulineE <elmahdi.mouline@live.fr>
2024-02-20 18:10:22 +0100

    Task 18. Update password --fix9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,7 +104,7 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
-            raise ValueError
+            return None
         hashed_password = _hash_password(password)
         self._db.update_user(user.id,
                              hashed_password=hashed_password,
