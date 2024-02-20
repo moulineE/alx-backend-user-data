e3dd4bde593d2adaa583b1dbb4abfdc9fd704971
moulineE <elmahdi.mouline@live.fr>
2024-02-20 15:10:16 +0100

    Task 12. Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,6 +59,20 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id):
+        """
+        method that find a user by session_id
+        :param session_id:
+        :return:
+        """
+        if session_id:
+            try:
+                user = self._db.find_user_by(session_id=session_id)
+                return user
+            except NoResultFound:
+                pass
+        return None
+
 
 def _hash_password(password: str) -> bytes:
     """
