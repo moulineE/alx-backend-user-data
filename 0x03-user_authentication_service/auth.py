5f45d4c08be9f354c7f621516b1b359f985c716f
moulineE <elmahdi.mouline@live.fr>
2024-02-22 04:32:14 +0100

    Task 18. Update password --fix adding comment

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,6 +61,11 @@ class Auth:
             return None
 
     def destroy_session(self, user_id: int) -> None:
+        """
+        method that destroy the session id in the db
+        :param user_id:
+        :return:
+        """
         try:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
